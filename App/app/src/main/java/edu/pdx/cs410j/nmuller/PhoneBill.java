afa1d8c0b410b8b9b8472ef19833f8ba4c8f2736
mu11ern <mu11ern@yahoo.com>
2022-08-03 19:56:00 -0700

    need to change the fragments to be non fragments

---
+++ App/app/src/main/java/edu/pdx/cs410j/nmuller/PhoneBill.java
@@ -0,0 +1,55 @@
+package edu.pdx.cs410j.nmuller;
+
+import java.util.ArrayList;
+
+import edu.pdx.cs410J.AbstractPhoneBill;
+
+public class PhoneBill extends AbstractPhoneBill<PhoneCall> {
+    /**
+     * final customer for the bill
+     */
+    private final String customer;
+
+    /**
+     * bill list for keeping track of calls
+     */
+    public ArrayList<PhoneCall> billOfCalls = new ArrayList<PhoneCall>() {
+    };
+
+    /**
+     * Creates a new <code>PhoneBill</code>
+     * @param customer
+     *        The name of the PhoneBill owner, only one per PhoneBill
+     */
+
+    public PhoneBill(String customer) {this.customer = customer;}
+
+    /**
+     *
+     * Returns a <code>String</code> of the PhoneBill customer
+     */
+    @Override
+    public String getCustomer() {
+        return this.customer;
+    }
+
+    /**
+     * Adds a call to billOfCalls for a specific customer bill
+     * @param call
+     *        Void return is successful, otherwise Exception thrown
+     */
+    @Override
+    public void addPhoneCall(PhoneCall call) {this.billOfCalls.add(call);}
+
+    /**
+     * @return Collection in form of vector
+     * returns the entire bill with all phone calls
+     */
+    @Override
+    public ArrayList<PhoneCall> getPhoneCalls() {return this.billOfCalls;}
+
+
+}
+
+
+
